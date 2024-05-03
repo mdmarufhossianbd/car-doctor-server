d1f51456c9e9238582bc8be321375edc9df8f9b7
Maruf Hossain <mdmarufh974@gmail.com>
2024-05-03 17:52:06 +0600

    delete api add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +27,48 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const carDoctorServiceCollection = client.db('carDoctorDB').collection('services')
+    const bookingCollection = client.db('carDoctorDB').collection('booking')
+
+
+    app.post('/booking', async(req, res)=>  {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
+    app.get('/booking', async(req, res)=>{
+      let query = {};
+      if(req.query?.email) {
+        query = {email : req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.delete('/booking/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get('/service', async(req, res)=>{
+      const cursor = carDoctorServiceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/service/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await carDoctorServiceCollection.findOne(query);
+      res.send(result);
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
